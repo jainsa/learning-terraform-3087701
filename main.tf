20b635a7297b7b7c925b6ad770f945334b96686a
jainsa <44487492+jainsa@users.noreply.github.com>
2022-12-17 18:38:28 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2b", "us-west-2c", "us-west-2d"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
@@ -45,7 +45,7 @@ module "blog_sg" {
   version             = "4.13.0"
   name                = "blog_new_sg"
 
-  vpc_id              = module.vpc.public_subnets[0].id
+  vpc_id              = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
